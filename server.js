38bd71cfbfdcce53510792e4fd595aa2012360c7
Norman Dube <ndubewebdesigner@gmail.com>
2020-09-08 21:23:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const cors = require('cors')
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
 const { Client } = require('pg');
+const nodemailer = require("nodemailer");
 
 
 app.use(cors())
 app.use(express.static('client/www'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
 
 
 // const config = {
@@ -113,6 +115,34 @@ app.delete('/api/movies/:movieId', (req, res, next) => {
 })
 
 app.get('/api/email', (req, res, next) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: testAccount.user, // generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"Fred Foo 👻" <foo@example.com>', // sender address
+    to: "ndubewebdesigner@gmail.com, baz@example.com", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
   res.status(200).json({message: 'Successfully Deleted Movie'});
 })
 
